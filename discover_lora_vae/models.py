5ff7c2ebe3d4e80f4b412afca8e69a9b19137b31
SonicCodes <seidramimohammed@gmail.com>
2024-06-09 05:49:23 +0300

    Changes in models for SAE and trainer for SAE
    new module SparseAE

--- discover_lora_vae/models.py
+++ discover_lora_vae/models.py
@@ -6,7 +6,7 @@ import sys
 sys.path.append('..')
 
 from common.models import ChunkFanOut, DiTBlockNoAda, AttentionResampler, Resnet
-
+import torch.nn.functional as F
 
 class Encoder(torch.nn.Module):
     def __init__(self, data_dim=1_365_504, model_dim=256, ff_mult=3, in_proj_chunks=1, act=torch.nn.SiLU, num_layers=6, latent_dim=None):
@@ -60,6 +60,62 @@ class LoraVAE(torch.nn.Module):
         eps = torch.randn_like(std)
         z = mean + eps * std
         return self.decoder(z), mean, logvar
+
+
+
+class SparseAE(nn.Module):
+    def __init__(self, expansion_factor=6, l0_alpha=3e-3):
+        super(SparseAE, self).__init__()
+        latent_dim = 512 
+        self.l0_alpha = l0_alpha
+        expanded_dim = round(latent_dim * expansion_factor)
+
+        self.dictionary_size = expanded_dim
+        self.up = nn.Linear(latent_dim, expanded_dim, bias=False)
+        self.down = nn.Linear(expanded_dim, latent_dim, bias=False)
+        self.apply(self._init_weights)
+
+    def initiate_vae(self,vae_weights_path,  **vae_params): # initiate the base VAE, make sure same weight is used during training and inference of this SAE
+        self.vae = LoraVAE(**vae_params)
+        self.vae.load_state_dict(torch.load(vae_weights_path))
+        self.freeze_vae()
+       
+
+    def normalize_dictionary(self):
+        self.down.weight.data = F.normalize(self.down.weight.data, p=2, dim=0)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            if module is self.down:
+                nn.init.orthogonal_(module.weight)
+            else:
+                nn.init.kaiming_normal_(module.weight, nonlinearity='relu') 
+            if module.bias is not None:
+                module.bias.data.fill_(0.01)
+    
+    def freeze_vae(self):
+        for param in self.vae.parameters():
+            param.requires_grad = False
+    
+    def forward(self, x):
+        with torch.no_grad():
+            mu,logvar = self.vae.encoder(x)
+
+        dense_dictionary = self.up(mu)
+        dictionary = F.relu(dense_dictionary)
+        self.normalize_dictionary()
+        
+        recon_x = self.down(dictionary)
+
+        eps = 1e-3
+        
+        l0_loss = ((F.sigmoid((dense_dictionary - eps) * 1000.0) ).sum() / self.dictionary_size) * self.l0_alpha  # approximated l0 loss
+
+            
+        return recon_x, mu, dictionary, l0_loss
+
+
+
 
 
 class Discriminator(torch.nn.Module):
---
+++ discover_lora_vae/train_sparse.py
@@ -0,0 +1,307 @@
+import copy
+import math
+import os
+import torch
+import torch.nn.functional as F
+import torch.utils.checkpoint
+from accelerate.logging import get_logger
+from tqdm.auto import tqdm
+import wandb
+import sys
+sys.path.append('..')
+import matplotlib.pyplot as plt
+
+
+from common.loras import patch_lora
+from common.utils import make_weight_vector, augmentations, rand_merge
+from common.train_utils import (
+    init_train_basics,
+    log_validation,
+    save_model,
+    unwrap_model,
+    get_optimizer,
+    more_init,
+    resume_model
+)
+
+from types import SimpleNamespace
+from discover_lora_vae.models import SparseAE
+from torch.utils.data import Dataset
+import random
+
+import numpy as np
+from scipy.stats import johnsonsu, norm
+
+
+def dist(num_samples=10_000, mean=0, std=1, kurtosis=3, skewness=0.0):
+    johnson_su = johnsonsu.fit(np.random.normal(size=num_samples), fa=skewness, fb=np.sqrt(kurtosis))
+    samples = johnsonsu.rvs(*johnson_su, size=num_samples)
+    scaled_samples = std * (samples - np.mean(samples)) / np.std(samples) + mean
+
+    return scaled_samples
+
+class LoraDataset(Dataset):
+
+    def __init__(
+        self,
+        lora_bundle_path,
+        num_dataloader_repeats=20, # this could blow up memory be careful!
+    ):
+        self.lora_bundle = torch.load(lora_bundle_path)
+        self.lora_bundle = [make_weight_vector(state_dict) for state_dict in self.lora_bundle]
+        self.weight_dict = self.lora_bundle[0][1]
+        self.lora_bundle = [x[0] for x in self.lora_bundle] * num_dataloader_repeats
+        random.shuffle(self.lora_bundle)
+
+    def __len__(self):
+        return len(self.lora_bundle)
+
+    def __getitem__(self, index):
+        return self.lora_bundle[index]
+
+
+
+class DummyDataset(Dataset):
+    
+        def __init__(self, data_dim):
+            self.data_dim = data_dim
+    
+        def __len__(self):
+            return 1000
+    
+        def __getitem__(self, index):
+            return torch.randn(self.data_dim)
+
+
+def collate_fn(examples):
+    return torch.stack(examples)
+
+
+def get_dataset(args):
+    train_dataset = DummyDataset(1000)
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=args.train_batch_size,
+        shuffle=True,
+        collate_fn=collate_fn,
+        num_workers=args.dataloader_num_workers,
+    )
+
+    # Scheduler and math around the number of training steps.
+    overrode_max_train_steps = False
+    num_update_steps_per_epoch = math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
+
+    return train_dataset, train_dataloader, num_update_steps_per_epoch
+
+
+default_arguments = dict(
+    data_dir="/home/ubuntu/AutoLoRADiscovery/lora_bundle.pt",
+    output_dir="model-output-sp",
+    seed=None,
+    train_batch_size=128,
+    max_train_steps=30_000,
+    num_dataloader_repeats=100,
+    checkpointing_steps=500,
+    # resume_from_checkpoint="/home/ubuntu/AutoLoRADiscovery/discover_lora_vae/model-output-sp/checkpoint-sph-15500",
+    resume_from_checkpoint=None,
+    gradient_accumulation_steps=1,
+    gradient_checkpointing=False,
+    learning_rate=2.0e-3,
+    lr_scheduler="linear",
+    lr_warmup_steps=500,
+    lr_num_cycles=1,
+    lr_power=1.0,
+    dataloader_num_workers=9,
+    use_8bit_adam=True,
+    adam_beta1=0.9,
+    adam_beta2=0.98,
+    adam_weight_decay=1e-9,
+    adam_epsilon=1e-08,
+    max_grad_norm=1.5,
+    report_to="wandb",
+    mixed_precision="bf16",
+    allow_tf32=True,
+    logging_dir="logs",
+    local_rank=-1,
+    num_processes=1,
+
+    data_dim = 1_365_504,
+
+    kld_weight = 0.2,
+
+    lora_std = 0.0152,
+    use_wandb=True
+)
+
+
+def log_activations(activations, step):
+    # Ensure activations are a NumPy array
+    if isinstance(activations, torch.Tensor):
+        activations = activations.detach().cpu().numpy()
+    
+    # Reduce the batch dimension by taking the maximum value over the batch
+    max_activations = np.max(activations, axis=0)
+    
+    # Log histogram of max activations
+    wandb.log({"activations_histogram": wandb.Histogram(max_activations)}, step=step)
+    
+    # Log max activations as a line plot
+    wandb.log({"max_activations": wandb.plot.line_series(
+        xs=list(range(len(max_activations))),
+        ys=[max_activations],  # Reshape to 2D array
+        keys=["Max Activations"],
+        title="Max Activations",
+        xname="Neuron"
+    )}, step=step)
+
+def train(args):
+    logger = get_logger(__name__)
+    args = SimpleNamespace(**args)
+    accelerator, weight_dtype = init_train_basics(args, logger)
+
+    lora_vae = SparseAE(
+        l1_alpha=0.05,
+        expansion_factor=6
+    )
+
+    lora_vae.initiate_vae(
+        "/home/ubuntu/AutoLoRADiscovery/discover_lora_vae/checkpoint-14000",
+        data_dim=args.data_dim,
+        model_dim=512,
+        ff_mult=3.0,
+        chunks=1,
+        # act=args.act,
+        encoder_layers=20,
+        decoder_layers=20
+         )
+
+    params_to_optimize = list(lora_vae.parameters())
+    train_dataset, train_dataloader, num_update_steps_per_epoch = get_dataset(args)
+    optimizer, lr_scheduler = get_optimizer(args, params_to_optimize, accelerator)
+    # weight_dict = train_dataset.weight_dict
+
+    lora_bundle = torch.load(args.data_dir)
+    lora_bundle = [make_weight_vector(state_dict) for state_dict in lora_bundle]
+    lora_bundle = [x[0] for x in lora_bundle]
+    lora_bundle = torch.stack(lora_bundle).cuda().to(torch.bfloat16) / args.lora_std
+
+    def get_a_lora(std=0.1, kurtosis=3.35):
+        weights = dist(num_samples=lora_bundle.shape[0], mean=0, std=std, kurtosis=kurtosis, skewness=0.0)
+        weights = torch.tensor(weights).to(lora_bundle.device).to(lora_bundle.dtype)
+        weights = weights - weights.mean()
+        a_lora = torch.sum(weights[:,None] * lora_bundle, dim=0)
+        return a_lora
+
+    # Prepare everything with our `accelerator`.
+    lora_vae, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
+        lora_vae, optimizer, train_dataloader, lr_scheduler
+    )
+
+    global_step = 0
+    if args.resume_from_checkpoint:
+        global_step = resume_model(lora_vae, args.resume_from_checkpoint, accelerator)
+
+    global_step, first_epoch, progress_bar = more_init(accelerator, args, train_dataloader, 
+                                                    train_dataset, logger, num_update_steps_per_epoch, 
+                                                    global_step, wandb_name="lora_vae")
+
+    def train_step(first=0.4, first_with_orig=0.01, second=0.4, second_w_orig=0.01, noise_std=0.01):
+        # generate random loras by mixing full dataset
+        batch = [get_a_lora() for _ in range(args.train_batch_size)]
+        batch = torch.stack(batch).to(torch.bfloat16)
+
+        orig_indices = torch.randperm(lora_bundle.shape[0])[:args.train_batch_size]
+        orig_batch = lora_bundle[orig_indices]
+
+        orig_batch = orig_batch.float()
+        batch = batch.float()
+
+        mask = torch.rand(batch.shape[0]) < first
+        if sum(mask) > 0:
+            perm = torch.randperm(batch.shape[0])
+            if random.random() < first_with_orig:
+                batch[mask] = rand_merge(batch[mask], orig_batch[perm][mask])
+            else:
+                batch[mask] = rand_merge(batch[mask], batch[perm][mask])
+
+
+        mask = torch.rand(batch.shape[0]) < second
+        if sum(mask) > 0:
+            perm = torch.randperm(batch.shape[0])
+            if random.random() < second_w_orig:
+                batch[mask] = rand_merge(batch[mask], orig_batch[perm][mask], slerp=True)
+            else:
+                batch[mask] = rand_merge(batch[mask], batch[perm][mask], slerp=True)
+
+        # add noise
+        noise_factors = torch.rand(batch.shape[0], 1).to(batch.device) * noise_std
+        batch = batch + torch.randn_like(batch) * noise_factors 
+
+        recon_x, ground_truth, sparse_logits, sparsity_loss = lora_vae(batch) 
+        mse_loss = F.mse_loss(recon_x.float(), ground_truth.float(), reduction="mean")
+
+
+        loss = (mse_loss) + sparsity_loss 
+
+        accelerator.backward(loss)
+        if accelerator.sync_gradients:
+            grad_norm = accelerator.clip_grad_norm_(params_to_optimize, args.max_grad_norm)
+        optimizer.step()
+        lr_scheduler.step()
+        optimizer.zero_grad(set_to_none=True)
+
+
+
+        return mse_loss, grad_norm, sparse_logits, sparsity_loss
+
+
+    grad_norm = 0
+    for i in range(args.max_train_steps):
+        with accelerator.accumulate(lora_vae):
+            mse_loss, grad_norm, sparse_logits, sp_loss = train_step() 
+
+        # Checks if the accelerator has performed an optimization step behind the scenes
+        if accelerator.sync_gradients:
+            progress_bar.update(1)
+            global_step += 1
+
+            if accelerator.is_main_process:
+                if global_step % args.checkpointing_steps == 0:
+                    save_path = os.path.join(args.output_dir, f"checkpoint-sph-{global_step}")
+                    torch.save(unwrap_model(accelerator, lora_vae).state_dict(), save_path)
+        
+        log_activations(sparse_logits, global_step)
+        count_non_zero = ((1-(torch.count_nonzero(sparse_logits) / (sparse_logits.size(0) * sparse_logits.size(1)))) * 100).detach().item()
+        max_val = sparse_logits.max().detach().item()
+        min_val = sparse_logits.min().detach().item()
+
+        logs = {
+            "mse_loss": mse_loss.detach().item(), 
+        "lr": lr_scheduler.get_last_lr()[0],
+         "grad_norm": grad_norm, 
+         "sp_loss": sp_loss.detach().item(),
+        "sp": count_non_zero,
+        "max": max_val,
+        "min": min_val,
+         }
+        progress_bar.set_postfix(**logs)
+        if args.use_wandb:
+            accelerator.log(logs, step=global_step)
+
+        if global_step >= args.max_train_steps:
+            break
+
+        if accelerator.is_main_process:
+            pass
+
+    # Save the lora layers
+    accelerator.wait_for_everyone()
+    if accelerator.is_main_process:
+        save_path = os.path.join(args.output_dir, "lora_vae.pth")
+        torch.save(unwrap_model(accelerator, lora_vae).state_dict(), save_path)
+
+    accelerator.end_training()
+
+
+if __name__ == "__main__":
+    train(default_arguments)
